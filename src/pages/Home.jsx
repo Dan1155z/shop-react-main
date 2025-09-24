a686ab03ee5c139fa06c27057c5974d03b723ef3
Dan1155z <dan1155m@yandex.ru>
2025-09-24 21:08:17 +0400

    commit1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 
 
+import { ProductList } from '../components/ProductList'
+import products from '../data/products'
+
 export const Home = () => {
-return (
-    <div>
-        
+  return (
+    <div className='container mx-auto p-4'>
+      <ProductList products={products} />
     </div>
-)
-
-
+  )
 }
