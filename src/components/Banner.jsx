a802f727d67c89d4cc8bb1d50b240b288dfe50d9
Dan1155z <dan1155m@yandex.ru>
2025-10-01 21:02:22 +0400

    Banner and home

---
+++ src/components/Banner.jsx
@@ -0,0 +1,15 @@
+export const Banner = () => {
+    return (
+        <div className="w-full bg-black text-white py-8 flex justify-center 
+        items-center rounded">
+            <div className="container mx-auto px-4 text-center">
+                <h2 className="text-3xl m-2">Добро пожаловать!</h2>
+                <p className="text-lg">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                    Assumenda voluptatem iusto nobis laudantium ratione eveniet accusantium necessitatibus. 
+                    Incidunt corrupti nostrum veniam voluptas quasi debitis magnam impedit sequi, nihil enim culpa!
+                </p>
+            </div>
+        </div>
+    )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,69 @@
-
-
+import { useEffect } from 'react'
 import { ProductList } from '../components/ProductList'
+import { Banner } from '../components/Banner'
 import products from '../data/products'
+import { useState } from 'react'
 
-export const Home = ({addToCart}) => {
+export const Home = ({ addToCart }) => {
+  const [query, setQuery] = useState('')
+  const [filteredProducts, setFilteredProducts] = useState(products)
+  const [sortOption, setSortOption] = useState('')
+  const [minPrice, setMinPrice] = useState('')
+  const [maxPrice, setMaxPrice] = useState('')
+
+  useEffect(() => {
+    let filtered = products
+
+    if (query.trim() !== '') {
+      filtered = filtered.filter(product =>
+        product.title.toLowerCase().includes(query.toLowerCase())
+      )
+
+    }
+
+    if (minPrice !== '') {
+      filtered = filtered.filter(
+        product => product.price >= parseFloat(minPrice)
+      )
+    }
+
+    if (maxPrice !== '') {
+      filtered = filtered.filter(
+        product => product.price >= parseFloat(maxPrice)
+      )
+    }
+
+    if (sortOption === 'price-asc') {
+      filtered.sort((a, b) => a.price - b.price)
+    } else if (sortOption === 'price-desc') {
+      filtered.sort((a, b) => b.price - a.price)
+    } else if (sortOption === 'name-desc') {
+      filtered.sort((a, b) => b.price - a.price)
+    } else if (sortOption === 'name-asc') {
+      filtered.sort((a, b) => b.price - a.price)
+    }
+    setFilteredProducts(filtered)
+  }, [query])
+
   return (
     <div className='container mx-auto p-4'>
-      <ProductList products={products} addToCart={addToCart} />
+      <Banner />
+      <div className='container mx-auto p-4'>
+        <h2 className='text-2xl mb-4 text-center'>Фильтрация</h2>
+        <div className='grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+          <div>
+            <label className="block text-gray mb-1" htmlFor='search'>Поиск</label>
+            <input
+              id='search'
+              type="text"
+              placeholder='Введите название'
+              value={query} onChange={e => setQuery(e.target.value)}
+              className='w-full px-4 py-2 border rounded focus:outline-none
+            focus:ring-2 focus:ring-blue-400' />
+          </div>
+        </div>
+      </div>
+      <ProductList products={filteredProducts} addToCart={addToCart} />
     </div>
   )
 }
