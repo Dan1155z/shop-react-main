0d58a554ad01066d632e8331e3f270b4a41010a5
Dan1155z <dan1155m@yandex.ru>
2025-10-03 20:56:12 +0400

    web2

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,13 +1,42 @@
 import { useState } from "react"
 
-export const Cart = ({cartItems, onUpdateQuantity, onRemove, onSaveCart}) => {
-    const[discountCode, setDiscountCode]= useState('')
-    const[applieDiscount, setApplieDiscount]=useState(0)
+export const Cart = ({ cartItems, onUpdateQuantity, onRemove, onSaveCart }) => {
+    const [discountCode, setDiscountCode] = useState('')
+    const [applieDiscount, setApplieDiscount] = useState(0)
 
-    const totalPrice = cartItems.reduce((sum, item)=> sum+item.price*item.quantity,0)
+    const totalPrice = cartItems.reduce((sum, item) => sum + item.price * item.quantity, 0)
     const discountPrice = totalPrice - totalPrice * appliedDiscount
 
     return (
-        
+        <div>
+            <h2>Ваша корзина!</h2>
+            {cartItems.length === 0 ? (
+                <p>Корзина пуста!</p>
+            ) : (
+                <>
+                    <div>
+                        {cartItems.map(item => (
+                            <div>
+                                <div>
+                                    <img src='' alt='' />
+                                    <div>
+                                        <h3></h3>
+                                        <p></p>
+                                    </div>
+                                </div>
+                                <div>
+                                    <button></button>
+                                    <span></span>
+                                    <button></button>
+                                </div>
+                                <div>
+                                    <button></button>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </>
+            )}
+        </div>
     )
 }
